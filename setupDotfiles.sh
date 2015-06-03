77e44b4bbcf5ac8127b0a69e03f09cfb1ea61152
w256 <w256@rdct.info>
2015-06-03 17:20:15 +0900

    modify:setupDotfiles.sh

--- setupDotfiles.sh
+++ setupDotfiles.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
+mkdir -p ~/dotfiles/local
+mkdir -p ~/.vim/undo
 ln -si ~/dotfiles/.zshrc ~/.zshrc
 ln -si ~/dotfiles/.vimrc ~/.vimrc
 ln -si ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sin ~/dotfiles/.vim/ ~/.vim
-mkdir -p ~/dotfiles/local
-mkdir -p ~/.vim/undo
 touch ~/dotfiles/local/.zshrc.local
 touch ~/dotfiles/local/.zshrc.local.alias
 touch ~/dotfiles/local/.zshrc.local.function
 touch ~/dotfiles/local/.bash.local.alias
-echo "dotfiles setup conpleted."
+echo "dotfiles setup completed."
